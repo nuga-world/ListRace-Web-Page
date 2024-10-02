4bac6b7c3d4a7948ee0c6479f6020282bfdaf33a
Rasky's MAC <mac@Nugas-MacBook-Pro.local>
2024-10-02 03:45:08 -0700

    updated

--- project.js
+++ project.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', () => {
 
     if (scrollTop > lastScrollTop) {
         // scrolling down
-        navbar.style.top = '-40px'; //Adjust this value to the height of your navbar
+        navbar.style.top = '-43px'; //Adjust this value to the height of your navbar
     } else {
         //Scrolling up
         navbar.style.top = '0';
@@ -14,3 +14,45 @@ window.addEventListener('scroll', () => {
     scrollTop= lastScrollTop
 })
 
+
+
+let slideIndex = 0;
+
+const slides = document.querySelectorAll('.single-testimonial-box')
+
+const totalSlides = slides.length;
+
+const showSlide =  (index) => {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('first-slide', i === index);
+    });
+    document.querySelector('.testimonial-carousel').style.transform = `translateX(-${index * 100}%)`;
+};
+
+const nextSlide = () => {
+    slideIndex = (slideIndex + 1) % totalSlides;
+    showSlide(slideIndex);
+};
+
+
+// document.querySelector('.next').addEventListener('click', nextSlide);
+
+// setInterval(() => { //THIS ALSO WORKS TO SET INTERVAL
+//     nextSlide()
+// }, 4000);
+
+// const prevSlide =() => {
+//     // slideIndex = (slideIndex - 1) % totalSlides;
+//                     //OR
+//     slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
+//     showSlide(slideIndex)
+// };
+
+// document.querySelector('.prev').addEventListener('click', prevSlide);
+
+// setInterval(nextSlide, 2000);
+
+                //OR
+// setInterval(() => { //THIS ALSO WORKS TO SET INTERVAL
+//     prevSlide()
+// }, 3000);
